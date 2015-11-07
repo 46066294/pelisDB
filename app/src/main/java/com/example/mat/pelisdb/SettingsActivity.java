34eb49cd863772797a56e87aa3b790e4c1906263
46066294 <46066294p@iespoblenou.org>
2015-11-07 14:57:52 +0100

    commit test vcs studio

--- app/src/main/java/com/example/mat/pelisdb/SettingsActivity.java
+++ app/src/main/java/com/example/mat/pelisdb/SettingsActivity.java
@@ -174,7 +174,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
                 || NotificationPreferenceFragment.class.getName().equals(fragmentName);
     }
-//
+/////
     /**
      * This fragment shows general preferences only. It is used when the
      * activity is showing a two-pane settings UI.
